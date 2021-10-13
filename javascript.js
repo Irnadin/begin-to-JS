c6dcc722329bebb46b0ae2557f408cb5785a0220
Irnadin <irynapoizdnyk@gmail.com>
2021-10-13 15:27:59 +0300

    js teoria

---
+++ javascript.js
@@ -0,0 +1,41 @@
+//Работа с переменными
+//console.log ('hello js')
+let bedinToStart = 'start  JS';
+//console.log (bedinToStart)
+ //const imya = prompt ('what is your name?');
+//console.log ('Your name is '+ imya);
+
+let firstnNum = 42;
+let secondNum = 2;
+//document.writeln ('Resalt', firstnNum + secondNum)
+firstnNum ++;
+//console.log (firstnNum)
+//console.log (Math.floor (2.05));
+//console.log (Math.ceil (2.05));
+//console.log (Math.round (2.55));
+
+//масивы
+//let color=['red', 'green','blue'];
+let color= new Array('red', 'green','blue')
+//можна добавить елемент в масив с помощью индекса (от 0), 
+//ниже добавили елемент gray
+color[3]='gray'
+//можно вместо индекса писать функцию push, добавляет елемент в конец масива
+color.push('rose')
+document.write(color[3]);
+//console.log (color [0] + color[4]);
+
+//Циклы
+//for(let i=100; i>=30 ;i-=5)
+   // console.log(i);
+  let i = 0;
+   while (i <10);
+   {
+     console.log ( i );
+    i++;
+}
+
+
+
+
+
